e61f7e1fe1862264854c2f4da8c5d809a309263d
Pedro de Melo Strabeli <pedrostrabeli@gmail.com>
2017-10-31 15:27:00 -0200

    Initial commit. Grid generated procedurally and selecting square with mouse hover

---
+++ Assets/Grid/TileMapMouse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TileMap))]
+public class TileMapMouse : MonoBehaviour {	
+
+	TileMap _tilemap;
+	Vector3 currentTileCoord;
+	public Transform selectionCube;
+
+	void Start() {
+		_tilemap = GetComponent<TileMap> ();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (ray, out hitInfo, Mathf.Infinity)) {
+			//TODO: normalize to the transform.worldToLocalMatrix
+
+			int x = Mathf.FloorToInt (hitInfo.point.x / _tilemap.tileSize);
+			int z = Mathf.FloorToInt (hitInfo.point.z / _tilemap.tileSize);
+
+			currentTileCoord.x = x;
+			currentTileCoord.z = z;
+			//Debug.Log (currentTileCoord);
+			selectionCube.transform.position = currentTileCoord;
+
+		} else {
+			// make cube disappear
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Debug.Log ("pressed");
+		}
+	}
+}
