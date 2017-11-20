97fc8880b2d66e08c29cab31b3537be06370deee
Pedro de Melo Strabeli <pedrostrabeli@gmail.com>
2017-11-20 17:46:48 -0200

    added first model and instantiated to follow mouse

--- Assets/GameManager/ButtonBehavior.cs
+++ Assets/GameManager/ButtonBehavior.cs
@@ -5,12 +5,36 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class ButtonBehavior : MonoBehaviour {
-	public GameObject prefabButton;
+	[SerializeField]
+	public GameObject house;
+
+	private BuildingPlacement bpl;
+	//public GameObject prefabButton;
+	public GameObject[] buildings;
+
+	Vector3 currentMouseCoord;
+
+	void Start() {
+		bpl = GetComponent<BuildingPlacement>();
+		//house = (GameObject)Resources.Load("Assets/Prefabs/house");
+		//cube.SetActive(false);
+		//bpl.setItem(house);
+	}
+
+
 	//public RectTransform ParentPanel;
 	//List<Action> funcList = new List<Action>();
 	//List<String> nameList = new List<String>();
 
-	public Transform _panel;
+	void Update() {
+		//if (building) {
+		//	if (Input.GetMouseButtonUp (0)) {
+		//	}
+		//	BuildBuilding ();
+		//}
+	}
+
+	//public Transform _panel;
 	/*void Start() {
 		RectTransform panelRectTransform = _panel.GetComponent<RectTransform>();
 
@@ -36,69 +60,85 @@ public class ButtonBehavior : MonoBehaviour {
 	}*/
 
 	public void BuildHouse () {
+		//Debug.Log (this.buildings[0]);
 		Debug.Log ("House Built");
-		BuildBuilding("houselvl1");
+		if (this.house == null)
+			this.house = Instantiate (house);
+		else
+			Destroy (house);
 	}
 
 	public void BuildApto () {
 		Debug.Log ("Apto Built");
-		BuildBuilding("houselvl2");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildCondo () {
-		Debug.Log ("Condo Built");	
-		BuildBuilding("houselvl3");
+		Debug.Log ("Condo Built");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildTower () {
 		Debug.Log ("Tower Built");
-		BuildBuilding("torre");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildBarracks () {
 		Debug.Log ("Barracks Built");
-		BuildBuilding("torre");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildCastle () {
 		Debug.Log ("Castle Built");
-		BuildBuilding("torre");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildParish () {
 		Debug.Log ("Parish Built");
-		BuildBuilding("churchlvl1");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildChurch () {
 		Debug.Log ("Church Built");
-		BuildBuilding("churchlvl2");
+		bpl.setItem (buildings [0]);
 	}
 
 	public void BuildCathedral () {
 		Debug.Log ("Castle Built");
-		BuildBuilding("churchlvl3");
+		bpl.setItem (buildings [0]);
 	}
 
-	public void BuildBuilding(String tag){
-		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);	
-		Vector3 mousepos = Input.mousePosition;
+	public void BuildBuilding(){
+		/*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (ray, out hitInfo, Mathf.Infinity)) {
+			//TODO: normalize to the transform.worldToLocalMatrix
 
-		if (Input.GetMouseButtonUp (0)) {
+			int x = Mathf.FloorToInt (hitInfo.point.x);/// _tilemap.tileSize);
+			int z = Mathf.FloorToInt (hitInfo.point.z);/// _tilemap.tileSize);
+
+			currentMouseCoord.x = x;
+			currentMouseCoord.z = z;
+			//Debug.Log (currentTileCoord);
+			cube.transform.position = currentMouseCoord;
+
+		} else {
+			cube.SetActive (false);// make cube disappear
+		}
 			//GameObject building = new GameObject();
 			//cube.AddComponent.<>();
 			//GameObject building = GameObject.FindGameObjectWithTag(tag);
 			//Image img = building.AddComponent<Image>();
 			//img.sprite = Resource.Load("torre", typeof(Sprite)) as Sprite; 
 			//cube.transform.localScale = Vector3(1, 1, 1);
-			cube.transform.position = new Vector3(mousepos.x, mousepos.y, mousepos.z);
-		}
-		setBuildingInGrid (cube, mousepos);
+		//cube.transform.position = new Vector3(mousepos.x, 0, mousepos.z);
+
+		//setBuildingInGrid (cube, mousepos);*/
 	}
 
 	public void setBuildingInGrid(GameObject gameobj, Vector3 mousepos) {
 	
-		gameobj.transform.position = new Vector3(mousepos.x, mousepos.y, mousepos.z);
+		//gameobj.transform.position = new Vector3(mousepos.x, mousepos.y, mousepos.z);
 	}
 }
 
